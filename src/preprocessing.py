cb7d462029c85943695cbce2a52b550c0b09f7aa
Sahil Joshi <sail.joshi0604@gmail.com>
2026-06-10 05:05:21 +0530

    EDA & preprocessing

---
+++ src/preprocessing.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+
+def get_X_y(df) :
+    X = df.drop(["rv1" , "rv2"] , axis = 1)
+
+    X["Time"] = (X["date"]).str[11:16]
+
+    X["Hour"] = (X["Time"].str[0 : 2]).astype(int)
+    X["Min"] = (X["Time"].str[3 :]).astype(int)
+    X["Month"] = (X["date"].str[5 : 7]).astype(int)
+    X["Day"] = (X["date"].str[8 : 10]).astype(int)
+
+    X = X.drop(["date" , "Appliances" , "Time"] , axis = 1)
+
+    y = df["Appliances"]
+
+    y = np.log1p(y)
+
+    return X , y
